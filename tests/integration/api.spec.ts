1e2dd3c3f99a2f5617810e10bf82942b437df71d
hikod <hikmetdogrul00@gmail.com>
2024-12-09 08:25:00 -0500

    adding snapshot for visual

--- tests/integration/api.spec.ts
+++ tests/integration/api.spec.ts
@@ -3,5 +3,7 @@ import { test, expect } from '@playwright/test';
 test.describe('API Integration Tests', () => {
   test('GET ', async ({ request }) => {
     // TODO: implement API integration test
+
+    test.fail(); // This test is expected to fail
   });
 });
